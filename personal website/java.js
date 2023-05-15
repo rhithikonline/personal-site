898e3d1ee76cdd16021dfd62c5b3802beb99625c
RHITHIK <123656899+rhithikonline@users.noreply.github.com>
2023-05-15 14:59:39 +0530

    Add files via upload

---
+++ personal website/java.js
@@ -0,0 +1,17 @@
+var preson="john";
+let person1="smith";
+document.write(person);
+//number
+var number=1
+var number=1.999
+
+//boolean
+var js=true;
+var html=false;
+
+
+//undifinennd
+var test1;
+
+//null
+var test2=null;
